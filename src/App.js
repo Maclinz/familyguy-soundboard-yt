c541f24a22c0d89d54bf8daca6d3afd877c2e188
maclinz <tafadzwamucheke@gmail.com>
2023-01-26 18:59:48 +0000

    Soundboard Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import logo from './img/logo.png';
+import data from './data'
+import Character from './Components/Character';
 
 function App() {
+  const [characters] = useState(data)
+  const [activeChar, setActiveChar] = useState(characters[0])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="logo">
+        <img src={logo} alt="" />
+      </div>
+      <ul className="characters-con">
+        {
+          characters.map((char) => {
+            const {id, image} = char;
+            return <li key={id} className={`${activeChar.id === id ? 'active': ''}`} 
+              onClick={() => setActiveChar(char)} >
+              <div className="characters">
+                <img src={image} alt="" />
+              </div>
+            </li>
+          })
+        }
+      </ul>
+      <Character {...activeChar} />
     </div>
   );
 }
